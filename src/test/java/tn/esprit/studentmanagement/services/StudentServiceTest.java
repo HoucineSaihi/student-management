80cfb86845cbc294186cc583e06d4733eeda67dc
HoucineSaihi <houcinesaihi6@gmail.com>
2026-01-29 14:39:08 +0100

    pushed docker file

---
+++ src/test/java/tn/esprit/studentmanagement/services/StudentServiceTest.java
@@ -0,0 +1,33 @@
+package tn.esprit.studentmanagement.services;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import tn.esprit.studentmanagement.entities.Student;
+import tn.esprit.studentmanagement.repositories.StudentRepository;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class StudentServiceTest {
+
+    @Mock
+    private StudentRepository studentRepository;
+
+    @InjectMocks
+    private StudentService studentService;
+
+    @Test
+    void testGetAllStudents() {
+        System.out.println("TESSSSSSST...");
+                System.out.println("TESSSSSSST...");
+        System.out.println("TESSSSSSST...");
+
+    }
+}
